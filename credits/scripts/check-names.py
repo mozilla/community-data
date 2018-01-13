9e2cb513291e39580d143ca81c2348c568687f00
Ram Dayal Vaishnav <Ram@Tokirs-MacBook-Air.local>
2018-01-13 20:52:21 +0530

    python script update and README update with instructions

--- credits/scripts/check-names.py
+++ credits/scripts/check-names.py
@@ -1,8 +1,9 @@
 import csv
 import requests
 import sys
+import git
 
-def process_credits(lower_bound, upper_bound, names_to_be_added, names_added, emails, checkin):
+def process_credits(lower_bound, upper_bound, names_to_be_added, names_added, emails, checkin, repo):
 	
 	response_data_url  = "https://docs.google.com/spreadsheet/ccc?key=0AtLNtYDDyKsudFNTSFR2WlFuOFpUQ2N2bXpPLUFTUnc&output=csv"
 
@@ -26,20 +27,25 @@ def process_credits(lower_bound, upper_bound, names_to_be_added, names_added, em
 			if flag == False:
 				emails.append(to_add_name.split(',')[2])
 				names_added.append((',').join(to_add_name.split(',')[0:2]))
-				checkin.append("Name - {name} <{email}> <{citation}>;".format(name=to_add_name.split(',')[0], email=to_add_name.split(',')[2], citation=to_add_name.split(',')[3]))
+				checkin.append("{name} <{email}>: \"{citation}\"".format(name=to_add_name.split(',')[0], email=to_add_name.split(',')[2], citation=to_add_name.split(',')[3]))
 			else:
 				# If one entry is found to be existing in names.csv, it will simply ignore this and proceed with next one after printing this.
 				print("Found duplicate name - {name}".format(name=to_add_name.split(',')[0]), " - Skipping this entry and proceeding.")
 
-	with open('../names.csv', 'a') as names:
-		for name in names_added:
+	for name, commit_message in zip(names_added, checkin):
+		with open('../names.csv', 'a') as names:
 			names.write(name+'\n')
-		# Printing count and email ids at the end.
-		print("Total number of names added: ", len(emails))
-		print("Email IDs to send mails to: ", emails)
-		print("Commit message: \n", checkin)
+			names.close()
+		repo.git.add('./credits/names.csv')
+		repo.git.commit(m=commit_message)
+
+	# Printing count and email ids at the end.
+	print("Total number of names added: ", len(emails))
+	print("Email IDs to send mails to: ", emails)
+	print("Names of newly added contributors: ", names_added)
 
 if __name__ == "__main__":
+	repo = git.Repo('../../')
 	lower_bound = sys.argv[1]
 	upper_bound = sys.argv[2]
 	names_to_be_added = []
@@ -48,4 +54,4 @@ if __name__ == "__main__":
 	checkin = []
 	# lower_boud and upper_bound are substracted by 1 to ignore first heading row from excel file. 
 	# Providing 2 & 3 as inputs to this script will process first & second entry of excel sheet as they are the ones in row 2 & 3 of excel sheet.
-	process_credits(int(lower_bound)-1, int(upper_bound)-1, names_to_be_added, names_added, emails, checkin)
+	process_credits(int(lower_bound)-1, int(upper_bound)-1, names_to_be_added, names_added, emails, checkin, repo)
